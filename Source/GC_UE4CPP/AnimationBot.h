bb0f7606920314e6869a8b05aeea0a829fb1347d
ScoopIndustries <91889058+ScoopIndustries@users.noreply.github.com>
2022-01-31 21:31:40 +0100

    Correction scale + Anim

---
+++ Source/GC_UE4CPP/AnimationBot.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "AI/AIBotCharacter.h"
+#include "AnimationBot.generated.h"
+
+/**
+ * 
+ */
+UCLASS(transient, Blueprintable, hideCategories=AnimInstance, BlueprintType)
+class GC_UE4CPP_API UAnimationBot : public UAnimInstance
+{
+	GENERATED_BODY()
+
+public:
+	UAnimationBot();
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, category = "Generic")
+		float Speed;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, category = "Generic")
+		bool isCarry;
+		
+
+	
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+};
